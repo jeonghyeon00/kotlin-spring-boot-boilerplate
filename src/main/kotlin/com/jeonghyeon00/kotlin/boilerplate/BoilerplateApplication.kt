07a8add5373f0b4ce40b587f637e35251fb2c005
SongJeongHyeon <56068392+jeonghyeon00@users.noreply.github.com>
2022-10-01 11:43:07 +0900

    [ADD] EnableTransaction + set ddl-auto to update

--- src/main/kotlin/com/jeonghyeon00/kotlin/boilerplate/BoilerplateApplication.kt
+++ src/main/kotlin/com/jeonghyeon00/kotlin/boilerplate/BoilerplateApplication.kt
@@ -2,8 +2,10 @@ package com.jeonghyeon00.kotlin.boilerplate
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.transaction.annotation.EnableTransactionManagement
 
 @SpringBootApplication
+@EnableTransactionManagement
 class BoilerplateApplication
 
 fun main(args: Array<String>) {
