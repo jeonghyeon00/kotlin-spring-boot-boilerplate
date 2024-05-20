5d0f927cec8925bc7acab3a78bbd3cefa7820cfd
jeonghyeon00 <sjh00kr@naver.com>
2024-05-20 17:11:11 +0900

    refactor: refactor optional to nullable

--- src/main/kotlin/com/jeonghyeon00/kotlin/boilerplate/module/service/UserDetailsService.kt
+++ src/main/kotlin/com/jeonghyeon00/kotlin/boilerplate/module/service/UserDetailsService.kt
@@ -1,6 +1,7 @@
 package com.jeonghyeon00.kotlin.boilerplate.module.service
 
 import com.jeonghyeon00.kotlin.boilerplate.module.repository.UserRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.http.HttpStatus
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
@@ -15,7 +16,7 @@ import java.util.Collections
 class UserDetailsService(private val userRepository: UserRepository) :
     UserDetailsService {
     override fun loadUserByUsername(username: String): UserDetails {
-        val user = userRepository.findById(username).orElseThrow { HttpServerErrorException(HttpStatus.NOT_FOUND) }
+        val user = userRepository.findByIdOrNull(username) ?: throw HttpServerErrorException(HttpStatus.NOT_FOUND)
         val grantedAuthority: GrantedAuthority = SimpleGrantedAuthority(user.authority.toString())
         return User(user.userId, user.password, Collections.singleton(grantedAuthority))
     }
